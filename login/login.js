84a7bfda17f6571824cbf8c04cfb4c58032f1c22
luizaugustoti <ti.luiz.augusto@gmail.com>
2026-06-01 19:59:55 -0300

    system

--- login/login.js
+++ login/login.js
@@ -42,9 +42,8 @@ document.addEventListener('DOMContentLoaded', () => {
             // Evite dar console.log na senha em produção por motivos de segurança
         });
 
-        window.location.href = "../rescepcao/recep.html";
+        window.location.href = "/login/login.html";
 
-        // Aqui você integraria com sua chamada de API / fetch do backend
-        // exemplo: authService.login(usernameValue, passwordValue)...
+       
     });
 });
